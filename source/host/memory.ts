de2870ab4fc0f0d30b349e13e72c9beab720e7b2
youssefa123 <yabdelhady905@gmail.com>
2023-09-19 09:20:20 -0400

    Created a memory file

---
+++ source/host/memory.ts
@@ -0,0 +1,21 @@
+// Main memory for the system.
+
+module TSOS { 
+    
+    //memory array 
+    export class Memory {
+        
+        //array to represent memory
+        private data: Uint8Array;
+
+        
+    }
+
+    
+
+
+
+
+
+
+}
