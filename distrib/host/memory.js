b4f138ef57889e6f980cdc0ca5c478869a4ef10f
youssefa123 <yabdelhady905@gmail.com>
2023-09-20 21:59:14 -0400

    Added A memory file for later use

--- distrib/host/memory.js
+++ distrib/host/memory.js
@@ -1,43 +1,25 @@
 // Main memory for the system.
-
-module TSOS { 
-    
+var TSOS;
+(function (TSOS) {
     //memory array 
-    export class Memory {
-        
-        //array to represent memory
-        private data: Uint8Array;
-
+    class Memory {
         // Memory space that can be fully addressed using a 16-bit address bus.
-        constructor(size: number = 0x10000) {
+        constructor(size = 0x10000) {
             this.data = new Uint8Array(size);
         }
-
         // Reads a byte from the specified address.
         // address: The memory address from which to read.
         // Returns: The byte value at the given address.
-        read(address: number): number {
+        read(address) {
             return this.data[address];
         }
-
         // Writes a byte to the specified address.
         // address: The memory address to which to write.
         // value: The byte value to write.
-        write(address: number, value: number): void {
+        write(address, value) {
             this.data[address] = value;
         }
     }
-
-}
-
-        
-    
-
-    
-
-
-
-
-
-
-
+    TSOS.Memory = Memory;
+})(TSOS || (TSOS = {}));
+//# sourceMappingURL=memory.js.map
