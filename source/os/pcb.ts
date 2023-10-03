f67542e667c365f1183c15448b63c621e6e3fa42
youssefa123 <yabdelhady905@gmail.com>
2023-10-03 18:44:26 -0400

    fnished the pcb, I think and linked it in the html

--- source/os/pcb.ts
+++ source/os/pcb.ts
@@ -1,14 +1,13 @@
 module TSOS {
     export class pcb { 
         
-        
         private static currentPID = 0; //Current state that were in 
 
 
         public id: number;
         public PC: number = 0; // Program counter
         public memorySegment: number; //Memory segment to 
-        
+        instructionRegister: number = 0;
         public Acc: number = 0; // Accumulator
         public Xreg: number = 0; // X register
         public Yreg: number = 0; // Y register
@@ -17,6 +16,18 @@ module TSOS {
         
         constructor(segment: number) {
             this.id = pcb.currentPID++; // assigninging the  next available PID
+
+            //cpu variables initalized
+            this.PC = 0;
+            this.Acc = 0;
+            this.Xreg = 0;
+            this.Yreg = 0;
+            this.instructionRegister = 0;
+
+            //mem segment 
+
+
+
         }
     }
 }
