adeeb34e079534ceb1793cf10775dcb25a2d3ce1
youssefa123 <yabdelhady905@gmail.com>
2023-10-04 19:18:48 -0400

    Made memory accessor, with write

--- source/host/memoryAccessor.ts
+++ source/host/memoryAccessor.ts
@@ -1,1 +1,17 @@
+module TSOS {
 
+    export class MemoryAccessor {
+
+        constructor(private _memory: Memory) {} // Dependency Injection
+
+        public writeByte(address: number, value: number): void {
+            if (address < 0 || address >= 256) {
+                // Invalid memory address
+                throw new Error("Invalid memory address.");
+            }
+
+            this._memory.setMemoryValue(address, value);
+        }
+
+   }
+}
