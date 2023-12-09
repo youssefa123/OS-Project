664f456d4ce44da59897d5abc46059543de8d8de
youssefa123 <yabdelhady905@gmail.com>
2023-12-09 11:43:15 -0500

    Added functionality to create, write into files by adding block logic

--- distrib/os/diskSystemDeviceDriver.js
+++ distrib/os/diskSystemDeviceDriver.js
@@ -81,6 +81,93 @@ var TSOS;
             }
             console.log("Formated disk", this.tracks);
         }
+        getNextFreeBlock() {
+            for (let b = 1; b < this.blocks.length; b++) {
+                var myBlock = this.blocks[b];
+                if (myBlock.inUse == false) {
+                    return myBlock;
+                }
+            }
+            console.log("Failed to find a free block");
+            return null;
+        }
+        getNextFreeContentBlock() {
+            for (let b = 1; b < this.blocks.length; b++) {
+                var myBlock = this.blocks[b];
+                if (myBlock.inUse == false && myBlock.track > 0) {
+                    return myBlock;
+                }
+            }
+            console.log("Failed to find a free content block");
+            return null;
+        }
+        getFileBlock(name) {
+            for (var block of this.blocks) {
+                var found = true;
+                if (block.inUse == false) {
+                    found = false;
+                }
+                for (var i = 0; i < name.length; i++) {
+                    if (block.data[i] != name.charCodeAt(i)) {
+                        found = false;
+                    }
+                }
+                if (found == true) {
+                    return block;
+                }
+            }
+            return null;
+        }
+        getBlock(blockName) {
+            for (let block of this.blocks) {
+                if (block.name == blockName) {
+                    return block;
+                }
+            }
+            return null;
+        }
+        create(filename) {
+            var myBlock = this.getNextFreeBlock();
+            myBlock.inUse = true;
+            // writing the file name into the system
+            for (let i = 0; i < filename.length; i++) {
+                myBlock.data[i] = filename.charCodeAt(i);
+            }
+            var myContentBlock = this.getNextFreeContentBlock();
+            myContentBlock.inUse = true;
+            myBlock.nextName = myContentBlock.name;
+        }
+        write(filename, content) {
+            var myBlock = this.getFileBlock(filename);
+            if (myBlock == null) {
+                _StdOut.putText("Could not find file: " + filename);
+            }
+            var firstContentBlock = this.getBlock(myBlock.nextName);
+            var currentContentBlock = firstContentBlock;
+            while (content.length > 0) {
+                var nextCharacter = content.charAt(0);
+                content = content.substring(1);
+                var freespace = null;
+                for (var i = 0; i < currentContentBlock.data.length && freespace == null; i++) {
+                    if (currentContentBlock.data[i] == 0) {
+                        console.log("Found free space");
+                        freespace = i;
+                    }
+                }
+                // create a new content block
+                if (freespace == null) {
+                    console.log("No free spaces, adding new block");
+                    var nextBlock = this.getNextFreeContentBlock();
+                    nextBlock.inUse = true;
+                    currentContentBlock.nextName = nextBlock.name;
+                    currentContentBlock = nextBlock;
+                    freespace = 0;
+                }
+                currentContentBlock.data[freespace] = nextCharacter.charCodeAt(0);
+            }
+        }
+        read(filename) {
+        }
         krnKbdDriverEntry() {
             // Initialization routine for this, the kernel-mode Keyboard Device Driver.
             this.status = "loaded";
--- source/os/diskSystemDeviceDriver.ts
+++ source/os/diskSystemDeviceDriver.ts
@@ -100,6 +100,116 @@ const byteCount = 64;
             console.log("Formated disk", this.tracks);
         }
 
+        public getNextFreeBlock(){
+            for (let b = 1; b < this.blocks.length; b++){
+                var myBlock = this.blocks[b];
+                if (myBlock.inUse == false){
+                    return myBlock;
+                }
+            }
+            console.log("Failed to find a free block")
+            return null;
+        }
+        public getNextFreeContentBlock(){
+            for (let b = 1; b < this.blocks.length; b++){
+                var myBlock = this.blocks[b];
+                if (myBlock.inUse == false && myBlock.track > 0){
+                    return myBlock;
+                }
+            }
+            console.log("Failed to find a free content block")
+            return null;
+        }
+
+        public getFileBlock(name:string){
+
+            for (var block of this.blocks){
+                var found = true;
+                if (block.inUse == false){
+                    found = false;
+                }
+
+                for (var i = 0; i < name.length; i++){
+                    if (block.data[i] != name.charCodeAt(i)){
+                        found = false;
+                    }
+                }
+
+
+                if (found == true){
+                    return block;
+                }
+            }
+
+            return null;
+
+        }
+
+        public getBlock(blockName:string){
+            for (let block of this.blocks){
+                if (block.name == blockName){
+                    return block;
+                }
+            }
+            return null;
+        }
+
+
+
+        public create(filename:string){
+            var myBlock = this.getNextFreeBlock();
+            myBlock.inUse = true;
+            // writing the file name into the system
+            for (let i = 0; i < filename.length; i++){
+                myBlock.data[i] = filename.charCodeAt(i);
+            }
+
+            var myContentBlock = this.getNextFreeContentBlock();
+            myContentBlock.inUse = true;
+
+            myBlock.nextName = myContentBlock.name;
+
+        }
+
+        public write(filename:string, content: string){
+            var myBlock = this.getFileBlock(filename);
+            if (myBlock == null){
+                _StdOut.putText("Could not find file: "+filename);
+            }
+            var firstContentBlock = this.getBlock(myBlock.nextName);
+            var currentContentBlock = firstContentBlock;
+            
+            while (content.length > 0){
+                var nextCharacter = content.charAt(0);
+                content = content.substring(1);
+                var freespace = null;
+                for (var i = 0; i < currentContentBlock.data.length && freespace == null; i++){
+                    if (currentContentBlock.data[i] == 0){
+                        console.log("Found free space");
+                        freespace = i;
+                    }
+                }
+                // create a new content block
+                if (freespace == null){
+                    console.log("No free spaces, adding new block")
+                    var nextBlock = this.getNextFreeContentBlock();
+                    nextBlock.inUse = true;
+        
+                    currentContentBlock.nextName = nextBlock.name;
+                    currentContentBlock = nextBlock;
+                    freespace = 0;
+                }
+
+                currentContentBlock.data[freespace] = nextCharacter.charCodeAt(0);
+            }
+
+            
+        }
+
+        public read(filename:string){
+
+        }
+
         public krnKbdDriverEntry() {
             // Initialization routine for this, the kernel-mode Keyboard Device Driver.
             this.status = "loaded";
